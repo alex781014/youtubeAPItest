80dd44430170de65b098765d7573fd53cfe31659
“alex781014” <alex781014@gamil.com>
2022-09-15 12:21:29 +0800

    ytAPItest

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,34 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from '../apis/youtube';
+import VideoList from "./VideoList";
+import VideoDetil from "./VideoDetil";
 
 class App extends React.Component {
+    state = {videos:[],selectedVideo:null}
+
     onTermSubmit = async term =>{
-      await youtube.get('/search',{
+      const response = await youtube.get('/search',{
         params:{
             q:term,
         }
        });
+      this.setState({videos:response.data.items})
     }
 
+    onVideoSelect = (video) => {
+      this.setState({selectedVideo:video});
+    }
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetil video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
